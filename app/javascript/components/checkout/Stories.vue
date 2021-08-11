25c1731c3b78752c44531fdf5bc2d8ab77280888
vkhoa <khoa.le@flyingwhales.io>
2021-08-11 11:37:31 +0700

    margin stories

--- app/javascript/components/checkout/Stories.vue
+++ app/javascript/components/checkout/Stories.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="stories mt-5 col-md-6 col-12 p-0">
-    <div class="py-5 ps-5">
-      <img :src="cusomerStoriesImg" class="w-100"/>
+    <div class="py-5 ps-5 overflow-hidden">
+      <img :src="cusomerStoriesImg" class="w-100 ms-5"/>
     </div>
   </div>
 </template>

390e044834773096746755c2fcf611ae60a18576
Giang Nguyen <giangnguyen1089@gmail.com>
2021-08-15 14:26:59 +0700

    add stripe_customer_id to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_12_172230) do
+ActiveRecord::Schema.define(version: 2021_08_15_072624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2021_08_12_172230) do
     t.string "phone"
     t.integer "budget_type"
     t.text "settings"
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

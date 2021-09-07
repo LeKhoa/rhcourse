e020ea03b6bf79506091d5178f3f1e550403618f
vkhoa <khoa.le@flyingwhales.io>
2021-09-07 15:19:48 +0700

    send welcome email

---
+++ app/services/user_notifier_service.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class UserNotifierService < BaseService
+  attr_reader :user
+
+  def initialize(user)
+    @user = user
+  end
+
+  def send_welcome_email
+    UserMailer.welcome(user).deliver
+  rescue StandardError => e
+    Rollbar.error(e)
+    error!(e.message)
+  end
+end
---
+++ config/initializers/mailgun.rb
@@ -0,0 +1,5 @@
+# config/intializers/mailgun.rb
+
+Mailgun.configure do |config|
+  config.api_key = ENV['MAILGUN_API_KEY']
+end

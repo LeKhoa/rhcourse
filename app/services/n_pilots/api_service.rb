7ab83103bd3b5376f34e01635346b7d7016a9c97
vkhoa <khoa.le@flyingwhales.io>
2021-09-21 16:44:32 +0700

    create nerdpilots task

---
+++ app/services/n_pilots/api_service.rb
@@ -0,0 +1,33 @@
+module NPilots
+  class ApiService < BaseService
+
+    BASE_URL = 'https://app.nerdpilots.com/api/ra/'
+    ENDPONTS = {
+      get_suport: 'get_support',
+    }
+
+    attr_reader :user
+
+    def initialize(user)
+      @user = user
+    end
+
+    def get_support
+      query = {
+        name: user.name,
+        email: user.email,
+      }
+      response = HTTParty.post("#{BASE_URL}#{ENDPONTS[:get_suport]}", query: query)
+      return response_error!(response) if !response_success?(response)
+      response
+    rescue => e
+      execute_error!(e)
+    end
+
+    protected
+
+    def response_success?(response)
+      response.code == 200 && response['status'] == 'success'
+    end
+  end
+end
